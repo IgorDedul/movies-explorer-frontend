db1864f3aa34cd9e7b085929129418921871eba2
IgorDedul <dedul_igor@mail.ru>
2023-09-28 00:01:55 +0300

    fix: correct after review1 part 3

--- src/components/Auth/AuthInput/AuthInput.js
+++ src/components/Auth/AuthInput/AuthInput.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './AuthInput.css';
 
-const AuthInput = () => {
+const AuthInput = ({
+  labelText,
+  error,
+  isLoading,
+  ...inputAttributes
+}) => {
   return (
     <label className='auth__input-container'>
-      <span className='auth__input-label'></span>
+      <span className='auth__input-label'>{labelText}</span>
       <input         
-        className='auth__input auth__input_incorrect'
+        className={`auth__input ${error ? 'auth__input_incorrect' : ''}`}
+        disabled={isLoading}
+        {...inputAttributes} 
       />
       <span className='auth__input-error'></span>
     </label>
