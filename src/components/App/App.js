65889a961c8d12af1635144eb98faee0b670a9a0
IgorDedul <dedul_igor@mail.ru>
2023-10-05 23:53:26 +0300

    feat: realisation base funnction app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,28 +21,144 @@ import Popup from '../Popup/Popup';
 
 const App = () => {
 
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+  const [savedMovies, setSavedMovies] = useState([]);
+  const [popupTitle, setPopupTitle] = useState('');
+  const [isOpenPopup, setIsOpenPopup] = useState(false);
 
+  const navigate = useNavigate();
 
+  // Открытие попапа
+  const openPopup = (popupText) => {
+    setPopupTitle(popupText);
+    setIsOpenPopup(true);
+  };
+
+  // Закрытие попапа
+  const closePopup = () => {
+    setIsOpenPopup(false);
+    setPopupTitle('');
+  };
+
+  // Авторизация токеном через локальное хранилище
+  const handleLogin = (token) => {
+    localStorage.setItem('token', token);
+    handleCheckToken();
+  };
+
+  // Выход из авторизации
+  const handleLogout = () => {
+    setCurrentUser({});
+    setLoggedIn(false);
+    localStorage.removeItem('token');
+    localStorage.removeItem('searchText');
+    localStorage.removeItem('areShortiesSeleted');
+    localStorage.removeItem('foundMovies');
+    navigate('/');
+  };
+
+  // Проверка токена
+  const handleCheckToken = () => {
+    const token = localStorage.getItem('token');
+
+    mainApi.checkToken(token)
+      .then((user) => {
+        if (user.data) {
+          setCurrentUser(user.data);
+          setLoggedIn(true);
+          navigate('/movies');
+
+          return mainApi.getSavedMovies()
+            .then((movies) => {
+              setSavedMovies(movies);
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        }
+      })
+      .catch((err) => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('searchText');
+        localStorage.removeItem('areShortMoviesSelected');
+        localStorage.removeItem('foundMovies');
+        setCurrentUser({});
+        console.error(err);
+      });
+  };
+
+  // Обновление профиля
+  const handleUpdateUser = (userInfo) => {
+    setCurrentUser(userInfo);
+    openPopup('Вы успешно обновили профиль!');
+  };
+
+  // Сохранение фильмов
+  const handleSaveMovie = (movieData) => {
+    mainApi.saveMovie(movieData)
+      .then((savedMovie) => {
+        if (savedMovie) {
+          setSavedMovies((movies) => [...movies, savedMovie]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  // Отмена сохранения фильмов
+  const handleUnsaveMovie = (movieId) => {
+    mainApi.unsaveMovie(movieId)
+      .then(() => {
+        setSavedMovies(savedMovies.filter((movie) => {
+          return movie._id !== movieId;
+        }));
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    handleCheckToken();
+  }, []);
+
+  // Закрытие попапа нажатием клавиши Esc
+  useEffect(() => {
+    if (isOpenPopup) {
+      const handleEscKeydown = (evt) => {
+        if (evt.key === 'Escape') {
+          closePopup();
+        }
+      };
+
+      document.addEventListener('keydown', handleEscKeydown);
+
+      return () => {
+        document.removeEventListener('keydown', handleEscKeydown);
+      }
+    }
+  }, [isOpenPopup]);
+
+  // Исключение хедера из указанных форм
   const RouteWrapper = ({path, element}) => {
-
     const routesWithoutHeader = [
       '/signin',
       '/signup',
       '*',
     ]
-
       return (
-
-              <>
-                {!routesWithoutHeader.includes(path) && <Header/>}
-                {element}
-              </>
-
-        )
-
-
+        <>
+          {!routesWithoutHeader.includes(path) && <Header/>}
+          {element}
+        </>
+      )
   }
+
   return (
+    <LoggedInContext.Provider value={loggedIn}>
+    <CurrentUserContext.Provider value={currentUser}>
     <div className={"app"}>
       <div className='content'>
 
@@ -82,6 +198,8 @@ const App = () => {
 
       </div>
     </div>
+    </CurrentUserContext.Provider>
+    </LoggedInContext.Provider>
   );
 };
 
